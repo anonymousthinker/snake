5156c846da2af9e5a59d2d6957e0d29a6d9d9e31
Gour Adhikary <157149036+anonymousthinker@users.noreply.github.com>
2024-11-21 14:51:21 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,46 +1,188 @@
+const SNAKE_POSITION = 5;
+const WIN_POSITION = 10;
+const LENGTH = 8;
+
+function delay(delaySpeed) {
+  for (let i = 0; i < delaySpeed; i++) { };
+}
+
+function animate(times, delaySpeed) {
+  if (times === 0) {
+    return;
+  }
+
+  if (times === 2) {
+    return animate(1, 800000000);
+  }
+
+  console.clear();
+  createDiceTwo();
+  delay(delaySpeed);
+  console.clear();
+  createDiceOne();
+  delay(delaySpeed);
+  console.clear();
+  createDiceFour();
+  delay(delaySpeed);
+  console.clear();
+  createDiceThree();
+  delay(delaySpeed);
+  console.clear();
+  createDiceSix();
+  delay(delaySpeed);
+  console.clear();
+  createDiceFive();
+  delay(delaySpeed);
+  console.clear();
+
+  return animate(times - 1, 400000000);
+}
+
+function createTopForAll(string) {
+  for (let index = 0; index < LENGTH; index++) {
+    string += '━';
+  }
+
+  return string + '┓';
+}
+
+function createBottomForAll(string) {
+  for (let index = 0; index < LENGTH; index++) {
+    string += '━';
+  }
+
+  return string + '┛';
+}
+
+function createDiceOne() {
+  const diceOneTop = createTopForAll('┏');
+  const diceOneMiddle = '┃        ┃\n┃   ⚪   ┃\n┃        ┃';
+  const diceOneBottom = createBottomForAll('┗');
+
+  console.log(diceOneTop);
+  console.log(diceOneMiddle);
+  console.log(diceOneBottom);
+}
+
+function createDiceTwo() {
+  const diceTwoTop = createTopForAll('┏');
+  const diceTwoMiddle = '┃        ┃\n┃ ⚪  ⚪ ┃\n┃        ┃';
+  const diceTwoBottom = createBottomForAll('┗');
+
+  console.log(diceTwoTop);
+  console.log(diceTwoMiddle);
+  console.log(diceTwoBottom);
+}
+
+function createDiceThree() {
+  const diceThreeTop = createTopForAll('┏');
+  const diceThreeMiddle = '┃⚪      ┃\n┃   ⚪   ┃\n┃      ⚪┃';
+  const diceThreeBottom = createBottomForAll('┗');
+
+  console.log(diceThreeTop);
+  console.log(diceThreeMiddle);
+  console.log(diceThreeBottom);
+}
+
+function createDiceFour() {
+  const diceFourTop = createTopForAll('┏');
+  const diceFourMiddle = '┃⚪    ⚪┃\n┃        ┃\n┃⚪    ⚪┃';
+  const diceFourBottom = createBottomForAll('┗');
+
+  console.log(diceFourTop);
+  console.log(diceFourMiddle);
+  console.log(diceFourBottom);
+}
+
+function createDiceFive() {
+  const diceFiveTop = createTopForAll('┏');
+  const diceFiveMiddle = '┃⚪    ⚪┃\n┃   ⚪   ┃\n┃⚪    ⚪┃';
+  const diceFiveBottom = createBottomForAll('┗');
+
+  console.log(diceFiveTop);
+  console.log(diceFiveMiddle);
+  console.log(diceFiveBottom);
+}
+
+function createDiceSix() {
+  const diceSixTop = createTopForAll('┏');
+  const diceSixMiddle = '┃⚪    ⚪┃\n┃⚪    ⚪┃\n┃⚪    ⚪┃';
+  const diceSixBottom = createBottomForAll('┗');
+
+  console.log(diceSixTop);
+  console.log(diceSixMiddle);
+  console.log(diceSixBottom);
+}
+
+function rollDice() {
+  animate(3, 400000000);
+  const diceValue = Math.ceil(Math.random() * 6);
+  switch (diceValue) {
+    case 1:
+      createDiceOne();
+      break;
+
+    case 2:
+      createDiceTwo();
+      break;
+
+    case 3:
+      createDiceThree();
+      break;
+
+    case 4:
+      createDiceFour();
+      break;
+
+    case 5:
+      createDiceFive();
+      break;
+
+    case 6:
+      createDiceSix();
+      break;
+  }
+
+  return diceValue;
+}
+
 function createMessage(playerPosition, dice) {
   const position = playerPosition + dice;
-  const initialMsgPartOne = '👦 position :' + playerPosition + '\n';
-  const initialMsgPartTwo = '🐍 position :' + 5 + '\n';
-  const initialMsgPartThree = '🏆 position :' + 6 + '\n';
+  const initialMsgPartOne = '👦 position :' + position + '\n';
+  const initialMsgPartTwo = '🐍 position :' + SNAKE_POSITION + '\n';
+  const initialMsgPartThree = '🏆 position :' + WIN_POSITION + '\n';
 
   console.log(initialMsgPartOne + initialMsgPartTwo + initialMsgPartThree);
 
-  if (position > 6) {
-    return 'You got ' + dice + ' and you exceeded game boundary! TRY AGAIN!\n';
+  if (position > WIN_POSITION) {
+    const messagePartOne = 'You got ' + dice + ', you exceeded game boundary!';
+    const messagePartTwo = ' Back to ' + playerPosition + '!\n';
+    return messagePartOne + messagePartTwo;
   }
 
-  if (position === 5) {
+  if (position === SNAKE_POSITION) {
     return 'Oops! You got bitten by a snake! Back to 0!\n';
   }
 
-  return 'You got ' + dice + ' ,now your current pos is ' + position + '\n'
+  return 'You got ' + dice + ' ,now your current pos is ' + position + '\n';
 }
 
-function gotSnake(playerPosition, dice) {
-  if (playerPosition === 5) {
-    playerPosition = 0;
-    createMessage(playerPosition, 0);
-    return playerPosition;
-  }
-
-  return playerPosition;
+function gotSnake(position) {
+  return position === SNAKE_POSITION;
 }
 
 function mainGame() {
   let playerPosition = 0;
-  let total = playerPosition;
 
-  while (total !== 6 && confirm('Roll the dice 🎲?')) {
-    const dice = (Math.round(Math.random() * 100) % 6) + 1;
-    const message = createMessage(playerPosition, dice);
-    console.log(message);
-    playerPosition += dice + playerPosition <= 6 ? dice : 0;
-    playerPosition = gotSnake(playerPosition, dice);
-    total = playerPosition;
+  while (playerPosition !== WIN_POSITION && confirm('Roll the dice 🎲?')) {
+    console.clear();
+    let dice = rollDice();
+    console.log(createMessage(playerPosition, dice));
+    dice = dice + playerPosition > 10 ? 0 : dice;
+    playerPosition += gotSnake(playerPosition + dice) ? 0 : dice;
   }
 
-  return playerPosition === 6 ? '🎉Congrats! You won!' : 'Scared? OK Bye then.';
+  return playerPosition === WIN_POSITION ? '🎉Congrats!You won!' : 'Come again';
 }
 
 console.log(mainGame());
